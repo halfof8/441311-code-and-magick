6bce4bd40a15d6df8eb79ef20dba15dfdf2ed0f9
Anton Sokolov <>
2018-04-09 16:14:18 +0300

    поменял " на ' и разделил код на 3 смысловые части

--- js/setup.js
+++ js/setup.js
@@ -1,46 +1,43 @@
-"use strict";
+'use strict';
 
 var WIZARD_NAMES = [
-  "Иван",
-  "Хуан Себастьян",
-  "Мария",
-  "Кристоф",
-  "Виктор",
-  "Юлия",
-  "Люпита",
-  "Вашингтон"
+  'Иван',
+  'Хуан Себастьян',
+  'Мария',
+  'Кристоф',
+  'Виктор',
+  'Юлия',
+  'Люпита',
+  'Вашингтон'
 ];
 var WIZARD_FAMILYNAMES = [
-  "да Марья",
-  "Верон",
-  "Мирабелла",
-  "Вальц",
-  "Онопко",
-  "Топольницкая",
-  "Нионго",
-  "Ирвинг"
+  'да Марья',
+  'Верон',
+  'Мирабелла',
+  'Вальц',
+  'Онопко',
+  'Топольницкая',
+  'Нионго',
+  'Ирвинг'
 ];
 var COLORS = [
-  "rgb(101, 137, 164)",
-  "rgb(241, 43, 107)",
-  "rgb(146, 100, 161)",
-  "rgb(56, 159, 117)",
-  "rgb(215, 210, 55)",
-  "rgb(0, 0, 0)"
+  'rgb(101, 137, 164)',
+  'rgb(241, 43, 107)',
+  'rgb(146, 100, 161)',
+  'rgb(56, 159, 117)',
+  'rgb(215, 210, 55)',
+  'rgb(0, 0, 0)'
 ];
-var EYECOLORS = ["black", "red", "blue", "yellow", "green"];
+var EYECOLORS = ['black', 'red', 'blue', 'yellow', 'green'];
 
 var wizards = [];
 
-//ищем setup и показываем
-var userDialog = document.querySelector(".setup");
-userDialog.classList.remove("hidden");
 
 //Функция смешивания массива
-var shuffle = function(array) {
-  var currentIndex = array.length,
-    temporaryValue,
-    randomIndex;
+function shuffle(array) {
+  var currentIndex = array.length;
+  var temporaryValue;
+  var randomIndex;
 
   // While there remain elements to shuffle...
   while (0 !== currentIndex) {
@@ -52,25 +49,17 @@ var shuffle = function(array) {
   }
 
   return array;
-};
+}
 
-//Рандомизируем массивы
+//Рандомизируем массивы и создаем массив магов
 WIZARD_NAMES = shuffle(WIZARD_NAMES);
 WIZARD_FAMILYNAMES = shuffle(WIZARD_FAMILYNAMES);
 COLORS = shuffle(COLORS);
 EYECOLORS = shuffle(EYECOLORS);
 
-//Ищем область для похожим магов
-var similarListElement = userDialog.querySelector(".setup-similar-list");
-
-var similarWizardTemplate = document
-  .querySelector("#similar-wizard-template")
-  .content.querySelector(".setup-similar-item");
-
-//Создаем массив магов
 for (var i = 0; i < 4; i++) {
   var wizard = {
-    name: WIZARD_NAMES[i] + " " + WIZARD_FAMILYNAMES[i],
+    name: WIZARD_NAMES[i] + ' ' + WIZARD_FAMILYNAMES[i],
     coatColor: COLORS[i],
     eyesColor: EYECOLORS[i]
   };
@@ -78,21 +67,35 @@ for (var i = 0; i < 4; i++) {
   wizards[i] = wizard;
 }
 
-//рисуем магов
+//ищем диалог настроек и показываем
+var userDialog = document.querySelector('.setup');
+userDialog.classList.remove('hidden');
+
+//Ищем область для похожих магов
+var similarListElement = userDialog.querySelector('.setup-similar-list');
+
+var similarWizardTemplate = document.querySelector('#similar-wizard-template')
+    .content
+    .querySelector('.setup-similar-item');
+
+
+//рендерим магов
 var renderWizard = function(wizard) {
   var wizardElement = similarWizardTemplate.cloneNode(true);
 
-  wizardElement.querySelector(".setup-similar-label").textContent = wizard.name;
-  wizardElement.querySelector(".wizard-coat").style.fill = wizard.coatColor;
-  wizardElement.querySelector(".wizard-eyes").style.fill = wizard.eyesColor;
+  wizardElement.querySelector('.setup-similar-label').textContent = wizard.name;
+  wizardElement.querySelector('.wizard-coat').style.fill = wizard.coatColor;
+  wizardElement.querySelector('.wizard-eyes').style.fill = wizard.eyesColor;
 
   return wizardElement;
 };
 
 var fragment = document.createDocumentFragment();
+
+//добавляем
 for (var i = 0; i < wizards.length; i++) {
   fragment.appendChild(renderWizard(wizards[i]));
 }
 similarListElement.appendChild(fragment);
 
-userDialog.querySelector(".setup-similar").classList.remove("hidden");
+userDialog.querySelector('.setup-similar').classList.remove('hidden');
